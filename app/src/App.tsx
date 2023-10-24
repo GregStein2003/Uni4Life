2a518a56e1a248b2cd3998d0bc111692971b2faf
GregStein2003 <gregorystein10@hotmail.com>
2023-10-24 00:26:55 -0300

    Initialize React with Vite

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Uni4Life
+      </div>
+    </>
+  )
+}
+
+export default App
